cc1ab7faf71368fb3f2ed2df3f5ddbbcd280f85c
kellyeryan <kellyeryan@gmail.com>
2019-11-16 13:46:28 -0500

    Finish refactoring views with helpers lab

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name; end
+  def artist_name
+    artist.name
+  end
 
-  def artist_name=(name); end
+  def artist_name=(name)
+    a = Artist.find_or_create_by(name: name)
+    self.artist = a
+  end
 end
